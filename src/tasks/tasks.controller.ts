0f684a25bca8faa691f9bbfe9eb95e45c1a59aa1
siihasann <mashasanr@gmail.com>
2025-11-14 08:27:58 +0700

    get the data sent in the request

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
@@ -8,12 +8,12 @@ export class TasksController {
   }
 
   @Post()
-  createTask() {
-    return 'This action creates a new task';
+  createTask(@Body() body: any) {
+    return body;
   }
 
   @Get('/:id')
-  getTask() {
-    return 'This action return a #task';
+  getTask(@Param('id') id: string) {
+    return `this actions returns a #${id} task`;
   }
 }

4f5e93aaee9558c5337127c718a3ef4855c12b47
siihasann <mashasanr@gmail.com>
2025-11-14 08:24:23 +0700

    initial commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+  @Get()
+  listTasks() {
+    return 'This action returns all tasks';
+  }
+
+  @Post()
+  createTask() {
+    return 'This action creates a new task';
+  }
+
+  @Get('/:id')
+  getTask() {
+    return 'This action return a #task';
+  }
+}

afa5e04c1079147138def41d1bf3f58d330ecc9a
siihasann <mashasanr@gmail.com>
2025-12-18 22:04:57 +0700

    creating service and repository

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,28 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class TasksRepository {
+  async findAll(): Promise<any> {
+    const data = await readFile('tasks.json', 'utf-8');
+    return JSON.parse(data);
+  }
+
+  async findOne(id: number): Promise<any> {
+    const data = await readFile('tasks.json', 'utf-8');
+    const tasks = JSON.parse(data);
+
+    return tasks.find((tasks: any) => tasks.id === id);
+  }
+
+  async create(task: string) {
+    const data = await readFile('tasks.json', 'utf-8');
+    const tasks = JSON.parse(data);
+
+    const newTask = {
+      id: tasks[tasks.length - 1].id + 1,
+      name: task,
+    };
+
+    tasks.push(newTask);
+    await writeFile('tasks.json', JSON.stringify(tasks, null, 2), 'utf-8');
+  }
+}
